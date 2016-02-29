5187d7778d24dc2219ad7dfe76c3f18d68cf42b5
Michael Knowles <mknowles@fulcrum.net>
2016-02-29 11:15:22 -0800

    Add yaml package requirement.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,19 @@
-# skytap_metadata set up a python script to allow us to 
+# skytap_metadata set up a python script to allow us to
 # collect the metadata json and make it available as facts in facter
 
 class skytap_metadata {
+    package { 'pyyaml':
+        provider => 'pip',
+        ensure   => 'installed',
+    }
+
     file { 'skytap_metadata.py':
-        ensure => file,
-        path   => '/etc/puppetlabs/facter/facts.d/skytap_metadata.py',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0755',
-        source => 'puppet:///modules/skytap_metadata/skytap_metadata.py';
+        ensure  => file,
+        path    => '/etc/puppetlabs/facter/facts.d/skytap_metadata.py',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        source  => 'puppet:///modules/skytap_metadata/skytap_metadata.py',
+        require => Package['pyyaml'],
     }
 }
